f3894cae0c96cd09f160f7f78bcd19d8354a45fd
F1j1tun1 <erkingalisaruar@gmail.com>
2025-12-16 15:15:16 +0500

    init commit

--- 1st Assignment/Main.java
+++ 1st Assignment/Main.java
@@ -2,22 +2,23 @@ public class Main {
     public static void main(String[] args) {
         Patient p1 = new Patient(1, "Saruar Yerkingali", 18, "A");
         Doctor d1 = new Doctor(1, "Dr. Smith", "Cardiology", 12);
-
-
         Appointment app1 = new Appointment(1, p1.getFullName(), d1.getFullName(), "16/12/2025");
 
-
         System.out.println(" ");
+
         System.out.println(p1);
         System.out.println(d1);
         System.out.println(app1);
 
+        System.out.println(" ");
         app1.reschedule("18/12/2025");
+        System.out.println("Appointment date has been changed!");
         System.out.println(app1);
 
+        System.out.println(" ");
         app1.cancel();
-        System.out.println(app1);
 
+        System.out.println(" ");
         System.out.println("Is patient minor? " + p1.isMinor());
         System.out.println("Patient age category: " + p1.getAgeCategory());
         System.out.println("Doctor experienced? " + d1.isExperienced());
